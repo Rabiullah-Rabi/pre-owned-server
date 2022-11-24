38b733aa2597be8f5d6d1598bd301e01b9a1e3d0
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-11-24 13:08:57 +0600

    api for save user info to database

--- index.js
+++ index.js
@@ -1,38 +1,61 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
-const app = express()
-const port = process.env.PORT || 1357
+const app = express();
+const port = process.env.PORT || 1357;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Database Connection
-const uri = process.env.DB_URI
+const uri = process.env.DB_URI;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-})
+});
 
 async function run() {
   try {
-    const categoryCollection = client.db('pre-owned').collection('category')
+    const categoryCollection = client.db("pre-owned").collection("category");
+    const usersCollection = client.db("easy-booking").collection("users");
+    // save user info
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
 
-    console.log('MongoDB connected ')
+      // generate jwt token
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+      });
+      res.send({ result, token });
+    });
+
+    console.log("MongoDB connected ");
   } finally {
   }
 }
 
-run().catch(err => console.error(err))
+run().catch((err) => console.error(err));
 
-app.get('/', (req, res) => {
-  res.send('Server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running`)
-})
+  console.log(`Server is running`);
+});
